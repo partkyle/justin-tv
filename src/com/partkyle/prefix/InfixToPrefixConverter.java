a35b9c8ca8e1a0c2963ea4651048e0feb18d8ceb
Kyle Partridge <partkyle@gmail.com>
2011-08-07 19:54:37 -0700

    Adding this to preserve history. This is not functioning completely.

--- src/com/partkyle/prefix/InfixToPrefixConverter.java
+++ src/com/partkyle/prefix/InfixToPrefixConverter.java
@@ -4,19 +4,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class InfixToPrefixConverter {
 	public static void main(String[] args) {
+		InfixToPrefixConverter converter = new InfixToPrefixConverter();
 		String result = null;
 		while (!"exit".equals(result)) {
-			result = getRawInput(">");
-			List<Number> operands = new ArrayList<Number>();
-			List<String> operation = new ArrayList<String>();
-			for (String s : result.split("[^\\d()+-/*^]")) {
-				System.out.println(s);
+			result = getRawInput("\n> ");
+			converter.convert(result);
+		}
+	}
+
+	public String convert(String result) {
+		List<String> output = new ArrayList<String>();
+		Stack<Operator> operators = new Stack<Operator>();
+		List<String> tokens = getTokens(result);
+		for (String token : tokens) {
+			Operator op = Operator.fromToken(token);
+			if (op != null) {
+				if (operators.size() > 0 && operators.peek() != null) {
+					if (op.precedence > operators.peek().precedence) {
+						operators.add(operators.size() - 1, op);
+					} else if (op.precedence <= operators.peek().precedence) {
+						output.add(operators.pop().character);
+						operators.add(op);
+					}
+				} else {
+					operators.add(op);
+				}
+			} else {
+				output.add(token);
 			}
 		}
+
+		output.add(operators.pop().character);
+
+		System.out.println(operators);
+
+		Collections.reverse(output);
+
+		for (String string : output) {
+			System.out.print(string);
+			System.out.print(" ");
+		}
+		System.out.println();
+		return "";
+	}
+
+	public List<String> getTokens(String expression) {
+		return Arrays.asList(expression.split("\\s+"));
 	}
 
 	public static String getRawInput(String prompt) {
