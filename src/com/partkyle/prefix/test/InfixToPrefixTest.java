4c953d2f9cb12ba48da648cbc7b6ff27643d9ef7
partkyle <partkyle@gmail.com>
2011-08-12 09:45:54 -0700

    fixed imports

--- src/com/partkyle/prefix/test/InfixToPrefixTest.java
+++ src/com/partkyle/prefix/test/InfixToPrefixTest.java
@@ -1,8 +1,8 @@
 package com.partkyle.prefix.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.partkyle.prefix.InfixToPrefixConverter;
